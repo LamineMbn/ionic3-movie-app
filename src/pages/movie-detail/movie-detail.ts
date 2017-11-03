209bc687475827bec00b455d1aeef44d45463c05
mlaminebendib <mohamed-lamine.bendib@consulting-for.accor.com>
2017-11-03 14:21:57 +0100

    add AlertController with fav and unfav

--- src/pages/movie-detail/movie-detail.ts
+++ src/pages/movie-detail/movie-detail.ts
@@ -1,6 +1,6 @@
 import { IMovie } from "../../interface/IMovie";
 import { Component } from "@angular/core";
-import { NavController, NavParams } from "ionic-angular";
+import { AlertController, NavController, NavParams } from "ionic-angular";
 
 @Component({
   selector: "page-movie-detail",
@@ -8,11 +8,41 @@ import { NavController, NavParams } from "ionic-angular";
 })
 export class MovieDetailPage {
   movie: IMovie;
+  favorite: boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private alertController: AlertController
+  ) {}
 
   ionViewDidLoad() {
     console.log("ionViewDidLoad MovieDetailPage");
     this.movie = this.navParams.data;
+  }
+
+  toggleFavorite(): void {
+    if (this.favorite) {
+      let confirm = this.alertController.create({
+        title: "Unfavorite?",
+        message: "Are you sure you want to unfav this movie?",
+        buttons: [
+          {
+            text: "Yes",
+            handler: () => {
+              this.favorite = false;
+              // TODO persist data
+            }
+          },
+          {
+            text: "No"
+          }
+        ]
+      });
+      confirm.present();
+    } else {
+      this.favorite = true;
+      // TODO persist data
+    }
   }
 }
